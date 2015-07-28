58ea7ba367aa955525d4e8268c4b53fdf4e1e183
Violet Siegius <fvcproductions@icloud.com>
2015-07-28 10:32:19 -0400

    Changing Navbar to include logo
    Also container is resizing in a weird way, still responsive though.
    
    Signed-off-by: Violet Siegius <fvcproductions@icloud.com>

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,9 +7,10 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', state: 'home' },
-                { label: 'About', state: 'about' },
-                { label: 'Documentation', state: 'docs' },
+                { label: 'Robopocalypse', state: 'home' },
+                { label: 'Browse', state: 'home' },
+                // { label: 'About', state: 'about' },
+                // { label: 'GitHub', state: 'docs' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
             ];
 
