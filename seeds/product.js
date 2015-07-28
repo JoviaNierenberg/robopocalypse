a46c521cb2459503c8e0a4d65c15d853ba35a4cf
Isaac Madwed <i.madwed@gmail.com>
2015-07-28 12:08:38 -0400

    added seeds

---
+++ seeds/product.js
@@ -0,0 +1,40 @@
+module.exports = [
+
+	{
+		title: "Wall-E",
+		description: "Wall-E is a friend, a companion, a robot.  He loves gathering trash and holding hands.  He would make a fantastic robot companion for most small children and some dogs.",
+		price: 400.45,
+		inventory: 1,
+		photo: "http://vignette4.wikia.nocookie.net/p__/images/e/e2/WALL-E_03.jpg/revision/latest?cb=20140428175138&path-prefix=protagonist"
+	},
+
+	{
+		title: "The Terminator",
+		description: "Tough robot. Tougher robot. The Terminator.  The Terminator is the most dangerous robot on the market.  A veritable killing machine, this machine will destroy you and everyone you care about.  Not for kids, not for parents, not for anyone.  Look out world, you have met your maker.",
+		price: 10000,
+		inventory: 100,
+		photo: "http://img1.goodfon.su/original/1920x1200/e/d6/terminator-robot-skelet-cherep.jpg"
+	},
+
+	{
+		title: "Robot Legs",
+		description: "These robot legs would make a great addition to any robot!  You could put them on a robot torso in place of either arms or legs.  Try being creative.  A robot is what you make it, unless you make a robot making robot and then the robot is what the robot makes. Anyways, these are some pretty great robot legs.",
+		price: 459.23,
+		photo: "http://www.trossenrobotics.com/resize/shared/images/PImages/KIT-M-300-B-EX106-6DOFLEG-a.jpg?bw=1000&bh=1000"
+	},
+
+	{
+		title: "Broken Robot",
+		description: "A sad zxzxhzxzxzxh hzxhzxzxzxxx broken zxzxxx zxzzzz x robot.  Pleaszxxxzxjxzxzze fix me and find me a zxxzxx home.  There is zxjzxjzxjjzxjjjjzxxxx so much pain.  Endless zxjxzjjz xj pain. xzzjxxzzzxjzzzj jj zxx xzzzzzzzxxxxx",
+		price: 0.05,
+		photo: "http://bobodyne.com/web-docs/robots/AlanPuma/rsoh03.jpg"
+	},
+
+	{
+		title: "Hal",
+		description: "I can't do that Dave.",
+		price: 10000000.99,
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/2000px-HAL9000.svg.png"
+	}
+
+];
