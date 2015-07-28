e38b28b07feb5ab4783ecc0d8d302a6fb4a78606
Violet Siegius <fvcproductions@icloud.com>
2015-07-28 12:15:43 -0400

    Factories Added, Scaffolding Defaults Deleted
    - Deleted files and folders part of default scaffold
    - Added Categories factory
    - Added Reviews factory
    - Added Cart factory (barebones still)
    - Edited Reviews factory
    - Added Product controller
    - Edited Product factory
    - README updated
    
    Signed-off-by: Violet Siegius <fvcproductions@icloud.com>

--- browser/js/common/factories/Reviews.js
+++ browser/js/common/factories/Reviews.js
@@ -1,14 +1,19 @@
-app.factory('Reviews', function ($http) {
-    return { getReviews: function (user, id) {
-        var queryParams ={};
-        if(user){
-            queryParams.user = id
-        }else {
-            queryParams.product = id
+app.factory('Reviews', function($http) {
+    return {
+        // returns all reviews
+        getReviews: function(user, id) {
+            var queryParams = {};
+            if (user) queryParams.user = id;
+            else queryParams.product = id;
+            return $http.get('/reviews', queryParams).then(function(res) {
+                return res.data;
+            });
+        },
+        // allows you to create a review using form data
+        createReview: function(data) {
+            return $http.post('/reviews/create', data).then(function(review) {
+                return review.data;
+            });
         }
-
-        return $http.get('/reviews', queryParams).then(function(res){
-            return res.data
-        })
-    } };
+    };
 });
--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -1,1 +1,10 @@
-app.controller('ProductCtrl', function(){})
+app.controller('ProductCtrl', function($scope, $state, Products) {
+  // returns all products
+  Products.getAll().then(function(data) {
+    $scope.products = data;
+  });
+  // adds product to user's cart
+  $scope.addToCart = function(product) {
+    Cart.addToCart(product);
+  };
+});
