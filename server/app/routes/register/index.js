1583eeb1e1bdeceef94df05b004db04f51e27df1
Violet Siegius <fvcproductions@icloud.com>
2015-07-29 11:57:18 -0400

    Login works, added a few tests to user model.
    Signed-off-by: Violet Siegius <fvcproductions@icloud.com>

---
+++ server/app/routes/register/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+router.post('/', function(req, res, next) {
+
+});
