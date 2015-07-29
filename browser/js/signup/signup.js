bfa3714ccf795291b9d84d63d1cb1f8e0b3cd27e
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-07-29 09:43:20 -0400

    sign up progress, but not done. Committing so that I can merge in changes made in master before continuing.

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -1,19 +1,25 @@
 app.config(function($stateProvider) {
     $stateProvider.state('signup', {
         url: '/signup',
-        templateUrl: 'js/signup/create.html',
+        templateUrl: 'js/signup/signup.html',
         controller: 'signupController'
     });
 });
 
-app.controller('signupController', function($scope, $window, AuthService, Signup, $state) {
-    $scope.createUser = function() {
-        console.log("creat user signupController");
-        Signup.createUser($scope.SignupForm).then(function(response) {
-            console.log("User creation response ", response);
-            delete $scope.SignupForm;
-            $state.go('home');
-        });
+app.controller('signupController', function($scope, $window, AuthService, $state) {
+    console.log('in signupController')
+    $scope.createUser = function(userData) {
+        console.log("create user entered. User data:", userData);
+        AuthService.signup(userData)
+        .then(function(signedupUser){
+            console.log('in .then')
+            $state.go('home')
+        })
+        // Signup.createUser($scope.SignupForm).then(function(response) {
+        //     console.log("User creation response ", response);
+        //     delete $scope.SignupForm;
+        //     $state.go('home');
+        // });
     };
     $scope.googleAuth = function() {
         console.log("Authenticating with Google OAuth2");
