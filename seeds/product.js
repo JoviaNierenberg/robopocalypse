2ce8f20171c392ea17ab84ed848d83b5f727c799
Isaac Madwed <i.madwed@gmail.com>
2015-07-29 16:59:20 -0400

    added a bit more seed data

--- seeds/product.js
+++ seeds/product.js
@@ -2,7 +2,7 @@ module.exports = [
 
 	{
 		title: "Wall-E",
-		description: "Wall-E is a friend, a companion, a robot.  He loves gathering trash and holding hands.  He would make a fantastic robot companion for most small children and some dogs.",
+		description: "Wall-E is a friend, a companion, a robot.  He loves gathering trash and holding hands.  He would make a fantastic robot companion for most small children and some dogs.\n\nOne thing you should know about Wall-E: he is quite the trickster!  Don't leave him alone with your untied shoes under the dinner table or with a whoopie cushion.  He can be kind, flavorful or even a garbage holder! Don't wait to buy Wall-E.  He is one of a kind (or so I've been told, there may be other prototypes on the market).  Seriously a great guy, 100% reliable robot.",
 		price: 400.45,
 		inventory: 1,
 		photo: "http://vignette4.wikia.nocookie.net/p__/images/e/e2/WALL-E_03.jpg/revision/latest?cb=20140428175138&path-prefix=protagonist"
@@ -10,7 +10,7 @@ module.exports = [
 
 	{
 		title: "The Terminator",
-		description: "Tough robot. Tougher robot. The Terminator.  The Terminator is the most dangerous robot on the market.  A veritable killing machine, this machine will destroy you and everyone you care about.  Not for kids, not for parents, not for anyone.  Look out world, you have met your maker.",
+		description: "Tough robot. Tougher robot. The Terminator.  The Terminator is the most dangerous robot on the market.  A veritable killing machine, this machine will destroy you and everyone you care about.  Not for kids, not for parents, not for anyone.  Look out world, you have met your maker. \nKaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching \n\nKaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching Kaching \nKaching \nKaching \nKaching \nKaching \nKaching \nKaching \nKaching",
 		price: 10000,
 		inventory: 100,
 		photo: "http://img1.goodfon.su/original/1920x1200/e/d6/terminator-robot-skelet-cherep.jpg"
@@ -18,7 +18,7 @@ module.exports = [
 
 	{
 		title: "Robot Legs",
-		description: "These robot legs would make a great addition to any robot!  You could put them on a robot torso in place of either arms or legs.  Try being creative.  A robot is what you make it, unless you make a robot making robot and then the robot is what the robot makes. Anyways, these are some pretty great robot legs.",
+		description: "These robot legs would make a great addition to any robot!  You could put them on a robot torso in place of either arms or legs.  Try being creative.  A robot is what you make it, unless you make a robot making robot and then the robot is what the robot makes. Anyways, these are some pretty great robot legs.\nIf you like robot legs or don't you should still buy these robot legs.  I didn't have a pair of robot legs and I can tell you that I regretted it every single day of my life.  Now I'm a lifeless husk and I use my robot legs every time I get up or sit down.\n\n Get your robot legs today!",
 		price: 459.23,
 		inventory: 56,
 		photo: "http://www.trossenrobotics.com/resize/shared/images/PImages/KIT-M-300-B-EX106-6DOFLEG-a.jpg?bw=1000&bh=1000"
@@ -26,7 +26,7 @@ module.exports = [
 
 	{
 		title: "Broken Robot",
-		description: "A sad zxzxhzxzxzxh hzxhzxzxzxxx broken zxzxxx zxzzzz x robot.  Pleaszxxxzxjxzxzze fix me and find me a zxxzxx home.  There is zxjzxjzxjjzxjjjjzxxxx so much pain.  Endless zxjxzjjz xj pain. xzzjxxzzzxjzzzj jj zxx xzzzzzzzxxxxx",
+		description: "A sad zxzxhzxzxzxh hzxhzxzxzxxx broken zxzxxx zxzzzz x robot.  Pleaszxxxzxjxzxzze fix me and find me a zxxzxx home.  There is zxjzxjzxjjzxjjjjzxxxx so much pain.  Endless zxjxzjjz xj pain. xzzjxxzzzxjzzzj jj zxx xzzzzzzzxxxxx\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.\nPleaszxxxzxjxzxzze fix me and find me a zxxzxx home.",
 		price: 0.05,
 		inventory: 15,
 		photo: "http://bobodyne.com/web-docs/robots/AlanPuma/rsoh03.jpg"
@@ -34,7 +34,7 @@ module.exports = [
 
 	{
 		title: "Hal",
-		description: "I can't do that Dave.",
+		description: "I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. I can't do that Dave. \nDave. \nDave.\nDave.",
 		price: 10000000.99,
 		inventory: 0,
 		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/2000px-HAL9000.svg.png"
