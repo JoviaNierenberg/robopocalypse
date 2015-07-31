6a8d0e62bc276a600b2a9ff64a8ed0d0b4a5fafb
Isaac Madwed <i.madwed@gmail.com>
2015-07-31 12:32:28 -0400

    reviews now show up in the most basic way

---
+++ browser/js/admin/directives/reviews/admin-review-line.js
@@ -0,0 +1,16 @@
+app.directive("adminReviewLine", function () {
+	return {
+		restrict: "E",
+		templateUrl: "js/admin/directives/reviews/admin-review-line.html",
+		controller: "AdminReviewLineCtrl",
+		scope: {
+			theReview: "=review"
+		}
+	}
+});
+
+app.controller("AdminReviewLineCtrl", function ($scope, Reviews){
+	$scope.delete = function () {
+		Reviews.deleteReview($scope.theUser._id);
+	}
+});
