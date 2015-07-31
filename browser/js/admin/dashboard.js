85b989664972c7521645383cc6930f20787c69cc
Isaac Madwed <i.madwed@gmail.com>
2015-07-31 10:38:08 -0400

    admin scaffolding

--- browser/js/admin/dashboard.js
+++ browser/js/admin/dashboard.js
@@ -6,28 +6,30 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('AdminCtrl', function ($scope, Products, Categories, Users, Orders, $stateParams){
-      $scope.deleteProduct = function (product) {
-        Products.deleteProduct(product._id);
-      };
-      $scope.getProducts = function() {
-        Products.getAll();
-      };
-      $scope.updateProduct = function(product) {
-        Products.updateProduct(product);
-      };
-      $scope.createProduct = function(product) {
-        Products.createProduct(product);
-      };
-      $scope.createCategory = function(category) {
-        Categories.createCategory(category);
-      };
-      $scope.deleteUser = function(user) {
-        User.deleteUser(user._id);
-      };
+app.controller('AdminCtrl', function ($scope, Products, Categories, User, Orders){
+  $scope.deleteProduct = function (product) {
+    Products.deleteProduct(product._id);
+  };
+  $scope.getProducts = function() {
+    Products.getAll();
+  };
+  $scope.updateProduct = function(product) {
+    Products.updateProduct(product);
+  };
+  $scope.createProduct = function(product) {
+    Products.createProduct(product);
+  };
+  $scope.createCategory = function(category) {
+    Categories.createCategory(category);
+  };
+  $scope.deleteUser = function(user) {
+    User.deleteUser(user._id);
+  };
+
+  $scope.show = function (what) {
+    $scope.active = what;
+  };
       // $scope.updateOrderStatus = function() {
       //   Orders.updateOrder(order);
       // };
-    
-  };
 });
