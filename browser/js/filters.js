7fc24608cc6febb1d5b7e214426a3f81eae79ca7
Violet Siegius <fvcproductions@icloud.com>
2015-08-03 01:51:35 -0400

    More Styling Done
    Signed-off-by: Violet Siegius <fvcproductions@icloud.com>

--- browser/js/filters.js
+++ browser/js/filters.js
@@ -1,14 +1,14 @@
-app.filter("shortdesc", function () {
+app.filter("shortdesc", function() {
     return function(description) {
-        if(description && description.length > 200) return description.slice(0, 200) + "...";
+        if (description && description.length > 200) return description.slice(0, 200) + "...";
         else return description;
     };
 });
 
-app.filter("getPrice", function () {
+app.filter("getPrice", function() {
     return function(price) {
-        return (price/100).toFixed(2).replace(/./g, function(c, i, a) {
-	        return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
-	    });
+        return (price / 100).toFixed(2).replace(/./g, function(c, i, a) {
+            return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+        });
     };
 });
