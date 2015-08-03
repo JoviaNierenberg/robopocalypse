30fb767f3b1acf16e55a804237047be557f1dc51
Isaac Madwed <i.madwed@gmail.com>
2015-08-03 14:20:51 -0400

    search works

--- server/app/routes/search/index.js
+++ server/app/routes/search/index.js
@@ -3,8 +3,19 @@ var router = require("express").Router();
 require("../../../db/models");
 var mongoose = require("mongoose");
 module.exports = router;
-var Product = mongoose.model("Product");
+var Promise = require("bluebird");
+var Product = Promise.promisifyAll(mongoose.model("Product"));
+var productSearch = function (string) {
+	return new Promise(function(resolve, reject){
+		Product.search(string, function (err, data){
+			if(err) reject(err);
+			else resolve(data);
+		})
+	});
+};
 
-router.get("/:searchString", function (req, res) {
-	var searches = req.params.searchString.split("%/20")
+router.get("/:searchString", function (req, res, next) {
+	productSearch(decodeURI(req.params.searchString)).then(function(products){
+		res.json(products);
+	}, next);
 });
