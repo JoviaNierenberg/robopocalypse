e4c0d0730c9513cd9cde2ebcf1565861e51af84a
Isaac Madwed <i.madwed@gmail.com>
2015-08-04 11:51:09 -0400

    email reset now works

--- server/app/routes/reset/index.js
+++ server/app/routes/reset/index.js
@@ -5,10 +5,9 @@ var User = mongoose.model("User");
 var Promise = require("bluebird");
 var _ = require("lodash");
 var path = require("path");
-var fs = require("fs");
+var fs = Promise.promisifyAll(require("fs"));
 var ejs = require("ejs");
 var mandrill = require("../sendEmail/mandrill.js");
-var promiseRead = Promise.promisifyAll(fs.readFile);
 
 module.exports = router;
 
@@ -17,7 +16,7 @@ router.get("/", function(req, res, next) {
 		.then(function(user){
 			req.query.user = user.full_name;
 			req.query.link = user.reset_link;
-			return promiseRead(path.join(__dirname, "../sendEmail/emails/passwordReset.ejs"), 'utf8');
+			return fs.readFileAsync(path.join(__dirname, "../sendEmail/emails/passwordReset.ejs"), 'utf8');
 		}).then(function (template){
 			var emailText = ejs.render(template, req.query)
 			mandrill.sendEmail(req.query.user , req.query.email, 'Robopocalypse', 'robopocalypse_admin@robopocalypse.com', 'Reset Your Password', emailText);
@@ -26,7 +25,6 @@ router.get("/", function(req, res, next) {
 });
 
 router.get("/key", function(req, res, next) {
-	console.log(req.query)
 	User.resetPass(req.query).then(function(user){
 		res.json(_.omit(user.toJSON(), ['salt', 'password']));
 	}, next);
