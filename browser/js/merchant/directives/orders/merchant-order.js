7ee0e20aab8beeecdbcef910601fa5d5e42873b7
Kevin Hoang <find.kevinhoang@gmail.com>
2015-08-04 16:03:55 -0400

    correct orders displayed on merchant

--- browser/js/merchant/directives/orders/merchant-order.js
+++ browser/js/merchant/directives/orders/merchant-order.js
@@ -12,17 +12,16 @@ app.controller("MerchantOrderCtrl", function ($scope, Orders, Products) {
 	Orders.getOrdersByMerchant($scope.merchant._id).then(function (orders) {
 		$scope.orders = orders;
 	}).then(function(){
-			$scope.orders.forEach(function(order){
-				for (var item in order.items){
-					Products.getOne(order.items[item].product).then(function(product){
+		$scope.orders.forEach(function(order){
+			for (var item in order.items){
+				Products.getOne(order.items[item].product).then(function(product){
 					order.items[item].product = product
 					order.items[item].price = (order.items[item].price/100).toFixed(2)
-					order.subtotal = (order.subtotal/100).toFixed(2)
+					order.subtotal = (order.subtotal/10000).toFixed(2)
 					order.items[item].total = order.items[item].quantity * order.items[item].price
 					order.date = new Date(Date.parse(order.date)).toLocaleString();
-					})
-				}
-			})
-		});
-	}
+				})
+			}
+		})
+	});
 });
